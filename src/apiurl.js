116142cbca90927b1058ac4c4098a368efb71290
aakash <p.saiaakash518@gmail.com>
2020-01-11 10:28:10 +0530

    switched to wss

--- src/apiurl.js
+++ src/apiurl.js
@@ -1,3 +1,3 @@
-export  const apiurl = 'http://localhost:5000'
+// export  const apiurl = 'http://localhost:5000'
 
-// export  const apiurl = 'https://mysterious-bayou-28479.herokuapp.com'
+export  const apiurl = 'https://mysterious-bayou-28479.herokuapp.com'
